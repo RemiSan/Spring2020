3c6f16e7c6d471c638a223cfee532e4c554dbcfa
Viotty Remi <remi.viotty@etudiant.univ-rennes1.fr>
2020-09-22 12:59:16 +0200

    dao changes

--- spring-boot-sample-simple-standalone/src/main/java/sample/simple/bank/BankAccountDao.java
+++ spring-boot-sample-simple-standalone/src/main/java/sample/simple/bank/BankAccountDao.java
@@ -14,6 +14,6 @@ public interface BankAccountDao extends JpaRepository<BankAccount, Long> {
    * Note that this method is not implemented and its working code will be
    * automagically generated from its signature by Spring Data JPA.
    */
-  public User findByEmail(String email);
+  public BankAccount findByEmail(String email);
 
 }
